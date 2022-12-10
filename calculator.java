febd114911fbc45fe1fc1aef91f23fa2bdfe806e
LalithaRavuri <95559628+LalithaRavuri@users.noreply.github.com>
2022-12-10 13:20:14 +0530

    Update calculator.java
    Changes done

--- calculator.java
+++ calculator.java
@@ -20,24 +20,24 @@ public class calculator implements ActionListener {
 	JTextArea textarea = new JTextArea(2,10);
 	
 	
-	JButton button1 = new JButton();
-	JButton button2 = new JButton();
-	JButton button3 = new JButton();
-	JButton button4 = new JButton();
-	JButton button5 = new JButton();
-	JButton button6 = new JButton();
-	JButton button7 = new JButton();
-	JButton button8 = new JButton();
-	JButton button9 = new JButton();
-	JButton button0 = new JButton();
+	JButton b1 = new JButton();
+	JButton b2 = new JButton();
+	JButton b3 = new JButton();
+	JButton b4 = new JButton();
+	JButton b5 = new JButton();
+	JButton b6 = new JButton();
+	JButton b7 = new JButton();
+	JButton b8 = new JButton();
+	JButton b9 = new JButton();
+	JButton b0 = new JButton();
 	
-	JButton buttonadd = new JButton();
-	JButton buttonsub = new JButton();
-	JButton buttonmul = new JButton();
-	JButton buttondiv = new JButton();
-	JButton buttonclear = new JButton();
-	JButton buttondot = new JButton();
-	JButton buttonequal = new JButton();
+	JButton badd = new JButton();
+	JButton bsub = new JButton();
+	JButton bmul = new JButton();
+	JButton bdiv = new JButton();
+	JButton bclear = new JButton();
+	JButton bdot = new JButton();
+	JButton bequal = new JButton();
 	
 	double number1,number2,result;
 	int add=0,mul=0,div=0,sub=0;
@@ -66,92 +66,92 @@ public class calculator implements ActionListener {
 		textarea.setPreferredSize(new Dimension(2,10));
 		textarea.setLineWrap(true);
 		
-		button1.setPreferredSize(new Dimension(100,38));
-		button1.setIcon(new ImageIcon("d:\\calculator\\imgs\\button1.png"));
+		b1.setPreferredSize(new Dimension(100,38));
+		b1.setIcon(new ImageIcon("d:\\calculator\\imgs\\button1.png"));
 		
-		button2.setPreferredSize(new Dimension(100,38));
-		button2.setIcon(new ImageIcon("d:\\calculator\\imgs\\button2.png"));
+		b2.setPreferredSize(new Dimension(100,38));
+		b2.setIcon(new ImageIcon("d:\\calculator\\imgs\\button2.png"));
 		
-		button3.setPreferredSize(new Dimension(100,38));
-		button3.setIcon(new ImageIcon("d:\\calculator\\imgs\\button3.png"));
+		b3.setPreferredSize(new Dimension(100,38));
+		b3.setIcon(new ImageIcon("d:\\calculator\\imgs\\button3.png"));
 		
-		button4.setPreferredSize(new Dimension(100,38));
-		button4.setIcon(new ImageIcon("d:\\calculator\\imgs\\button4.png"));
+		b4.setPreferredSize(new Dimension(100,38));
+		b4.setIcon(new ImageIcon("d:\\calculator\\imgs\\button4.png"));
 		
-		button5.setPreferredSize(new Dimension(100,38));
-		button5.setIcon(new ImageIcon("d:\\calculator\\imgs\\button5.png"));
+		b5.setPreferredSize(new Dimension(100,38));
+		b5.setIcon(new ImageIcon("d:\\calculator\\imgs\\button5.png"));
 		
-		button6.setPreferredSize(new Dimension(100,38));
-		button6.setIcon(new ImageIcon("d:\\calculator\\imgs\\button6.png"));
+		b6.setPreferredSize(new Dimension(100,38));
+		b6.setIcon(new ImageIcon("d:\\calculator\\imgs\\button6.png"));
 		
-		button7.setPreferredSize(new Dimension(100,38));
-		button7.setIcon(new ImageIcon("d:\\calculator\\imgs\\button7.png"));
+		b7.setPreferredSize(new Dimension(100,38));
+		b7.setIcon(new ImageIcon("d:\\calculator\\imgs\\button7.png"));
 		
-		button8.setPreferredSize(new Dimension(100,38));
-		button8.setIcon(new ImageIcon("d:\\calculator\\imgs\\button8.png"));
+		b8.setPreferredSize(new Dimension(100,38));
+		b8.setIcon(new ImageIcon("d:\\calculator\\imgs\\button8.png"));
 		
-		button9.setPreferredSize(new Dimension(100,38));
-		button9.setIcon(new ImageIcon("d:\\calculator\\imgs\\button9.png"));
+		b9.setPreferredSize(new Dimension(100,38));
+		b9.setIcon(new ImageIcon("d:\\calculator\\imgs\\button9.png"));
 		
-		button0.setPreferredSize(new Dimension(100,38));
-		button0.setIcon(new ImageIcon("d:\\calculator\\imgs\\button0.png"));
+		b0.setPreferredSize(new Dimension(100,38));
+		b0.setIcon(new ImageIcon("d:\\calculator\\imgs\\button0.png"));
 		
-		buttondot.setPreferredSize(new Dimension(100,38));
-		buttondot.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttondot.jpg"));
+		bdot.setPreferredSize(new Dimension(100,38));
+		bdot.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttondot.jpg"));
 		
-		buttonadd.setPreferredSize(new Dimension(100,38));
-		buttonadd.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttonadd.jpg"));
+		badd.setPreferredSize(new Dimension(100,38));
+		badd.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttonadd.jpg"));
 		
-		buttonsub.setPreferredSize(new Dimension(100,38));
-		buttonsub.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttonsub.jpg"));
+		bsub.setPreferredSize(new Dimension(100,38));
+		bsub.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttonsub.jpg"));
 		
-		buttonmul.setPreferredSize(new Dimension(100,38));
-		buttonmul.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttonmul.png"));
+		bmul.setPreferredSize(new Dimension(100,38));
+		bmul.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttonmul.png"));
 		
-		buttondiv.setPreferredSize(new Dimension(100,38));
-		buttondiv.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttondiv.jpg"));
+		bdiv.setPreferredSize(new Dimension(100,38));
+		bdiv.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttondiv.jpg"));
 		
-		buttonequal.setPreferredSize(new Dimension(100,38));
-		buttonequal.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttonequal.jpg"));
+		bequal.setPreferredSize(new Dimension(100,38));
+		bequal.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttonequal.jpg"));
 		
-		buttonclear.setPreferredSize(new Dimension(100,38));
-		buttonclear.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttonclear.png"));
+		bclear.setPreferredSize(new Dimension(100,38));
+		bclear.setIcon(new ImageIcon("d:\\calculator\\imgs\\buttonclear.png"));
 		
-		panel.add(button1);
-		panel.add(button2);
-		panel.add(button3);
-		panel.add(button4);
-		panel.add(button5);
-		panel.add(button6);
-		panel.add(button7);
-		panel.add(button8);
-		panel.add(button9);
-		panel.add(button0);
-		panel.add(buttondot);
-		panel.add(buttonadd);
-		panel.add(buttonsub);
-		panel.add(buttonmul);
-		panel.add(buttondiv);
-		panel.add(buttonequal);
-		panel.add(buttonclear);
+		panel.add(b1);
+		panel.add(b2);
+		panel.add(b3);
+		panel.add(b4);
+		panel.add(b5);
+		panel.add(b6);
+		panel.add(b7);
+		panel.add(b8);
+		panel.add(b9);
+		panel.add(b0);
+		panel.add(bdot);
+		panel.add(badd);
+		panel.add(bsub);
+		panel.add(bmul);
+		panel.add(bdiv);
+		panel.add(bequal);
+		panel.add(bclear);
 		
-		button1.addActionListener((ActionListener) this);
-		button2.addActionListener((ActionListener) this);
-		button3.addActionListener((ActionListener) this);
-		button4.addActionListener((ActionListener) this);
-		button5.addActionListener((ActionListener) this);
-		button6.addActionListener((ActionListener) this);
-		button7.addActionListener((ActionListener) this);
-		button8.addActionListener((ActionListener) this);
-		button9.addActionListener((ActionListener) this);
-		button0.addActionListener((ActionListener) this);
-		buttondot.addActionListener((ActionListener) this);
-		buttonadd.addActionListener((ActionListener) this);
-		buttonsub.addActionListener((ActionListener) this);
-		buttonmul.addActionListener((ActionListener) this);
-		buttondiv.addActionListener((ActionListener) this);
-		buttonequal.addActionListener((ActionListener) this);
-		buttonclear.addActionListener((ActionListener) this);
+		b1.addActionListener((ActionListener) this);
+		b2.addActionListener((ActionListener) this);
+		b3.addActionListener((ActionListener) this);
+		b4.addActionListener((ActionListener) this);
+		b5.addActionListener((ActionListener) this);
+		b6.addActionListener((ActionListener) this);
+		b7.addActionListener((ActionListener) this);
+		b8.addActionListener((ActionListener) this);
+		b9.addActionListener((ActionListener) this);
+		b0.addActionListener((ActionListener) this);
+		bdot.addActionListener((ActionListener) this);
+		badd.addActionListener((ActionListener) this);
+		bsub.addActionListener((ActionListener) this);
+		bmul.addActionListener((ActionListener) this);
+		bdiv.addActionListener((ActionListener) this);
+		bequal.addActionListener((ActionListener) this);
+		bclear.addActionListener((ActionListener) this);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
@@ -162,40 +162,40 @@ public class calculator implements ActionListener {
 			number2=0.0;
 			textarea.setText("");
 		}
-		if(source==button1) {
+		if(source==b1) {
 			textarea.append("1");
 		}
-		if(source==button2) {
+		if(source==b2) {
 			textarea.append("2");
 		}
-		if(source==button3) {
+		if(source==b3) {
 			textarea.append("3");
 		}
-		if(source==button4) {
+		if(source==b4) {
 			textarea.append("4");
 		}
-		if(source==button5) {
+		if(source==b5) {
 			textarea.append("5");
 		}
-		if(source==button6) {
+		if(source==b6) {
 			textarea.append("6");
 		}
-		if(source==button7) {
+		if(source==b7) {
 			textarea.append("7");
 		}
-		if(source==button8) {
+		if(source==b8) {
 			textarea.append("8");
 		}
-		if(source==button9) {
+		if(source==b9) {
 			textarea.append("9");
 		}
-		if(source==button0) {
+		if(source==b0) {
 			textarea.append("0");
 		}
-		if(source==buttondot) {
+		if(source==bdot) {
 			textarea.append(".");
 		}
-		if(source==buttonadd) {
+		if(source==badd) {
 			number1 = number_reader();
 			textarea.setText("+");
 			add=1;
@@ -203,7 +203,7 @@ public class calculator implements ActionListener {
 			sub=0;
 			mul=0;
 		}
-		if(source==buttondiv) {
+		if(source==bdiv) {
 			number1 = number_reader();
 			textarea.setText("/");
 			add=0;
@@ -211,7 +211,7 @@ public class calculator implements ActionListener {
 			sub=0;
 			mul=0;
 		}
-		if(source==buttonmul) {
+		if(source==bmul) {
 			number1 = number_reader();
 			textarea.setText("x");
 			add=0;
@@ -219,7 +219,7 @@ public class calculator implements ActionListener {
 			sub=0;
 			mul=1;
 		}
-		if(source==buttonsub) {
+		if(source==bsub) {
 			number1 = number_reader();
 			textarea.setText("-");
 			add=0;
@@ -227,7 +227,7 @@ public class calculator implements ActionListener {
 			sub=1;
 			mul=0;
 		}
-		if(source==buttonequal) {
+		if(source==bequal) {
 			number2 = number_reader();
 			if(add>0) {
 				result=number1+number2;
